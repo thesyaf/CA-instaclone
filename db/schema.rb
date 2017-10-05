8358f33568af0321211db8bf216cc6f579be598a
Syaf Djamil <the.syaf@gmail.com>
2017-10-05 15:23:05 +1100

    added in username functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005025852) do
+ActiveRecord::Schema.define(version: 20171005040303) do
 
   create_table "photos", force: :cascade do |t|
     t.text "image_data"
