9876aabd468ddd13fb8811f3891d758a15a4e35f
Syaf Djamil <the.syaf@gmail.com>
2017-10-05 14:38:03 +1100

    added username function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171004045043) do
+ActiveRecord::Schema.define(version: 20171005025852) do
 
   create_table "photos", force: :cascade do |t|
     t.text "image_data"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20171004045043) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
